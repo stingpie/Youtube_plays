b3c06a07bf4792f36dde86655d544564960dba11
Whomobiles <owen7miller@gmail.com>
2020-07-14 09:22:44 +0800

    Update Youtube alpha prototype a3.py

--- Youtube alpha prototype a3.py
+++ Youtube alpha prototype a3.py
@@ -1,13 +1,5 @@
 # Hi it's Whomobile and welcome to newbie programmer hour, theres gonna be A LOT of comments on this. 
 #---------------------------------------------------------
-# I used these Twitch plays examples of code for reference:
-# https://www.wituz.com/make-your-own-twitch-plays-stream.html
-# https://www.dougdougw.com/twitch-plays-code/twitchplays-py
-
-# I also looked at Pimanrules's code for YouTube Plays Super Mario Odyssey for reference too,
-# mosty for the system that allows commands to be queued:
-# https://gist.github.com/jsettlem/ca3274927f872f14fc8eae821a76e0cf
-
 # I use the pytchat module to find and read the Youtube livechat, it seems to work well.
 # I should probably look at the Youtube API for Python at some point but this is more easy.
 
